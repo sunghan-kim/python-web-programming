d8aa9426015ea9bca607baaeb1de99ee4df7a6ad
shkim4738 <shkim4738@gmail.com>
2020-12-30 19:57:39 +0900

    2.3 요청과 응답 다루기 - MultiDict

---
+++ 2-3_request-response/02_MultiDict.py
@@ -0,0 +1,17 @@
+# MultiDict
+#  - GET과 POST 메서드로 넘어온 데이터가 (key, value)으로 된 튜플을 요소로 가지는 리스트 데이터 타입이다.
+#  - 리스트 타입에서 일반적으로 제공하는 메서드가 아니라 딕셔너리 타입에서 제공하는 메서드를 제공한다.
+
+# MultiDict 데이터 타입이 제공하는 메서드들
+#  1) get
+#  2) getlist
+#  3) add
+#  4) setlist
+#  5) setdefault
+#  6) setlistdefault
+#  7) clear
+#  8) copy
+#  9) deepcopy
+#  10) pop
+#  11) poplist
+#  12) update
